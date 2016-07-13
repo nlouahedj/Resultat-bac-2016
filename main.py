d646cc65919b1e509911cb7d70caa5709ae9c0ec
Noureddine LOUAHEDJ <louahedj@noureddine.me>
2016-07-13 15:17:55 +0100

    transformed from csv to sqlite db

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import random
 import time
 import threading
 import re
+import sqlite3
 
 
 url = 'http://bac.onec.dz/index.php'
 
-tabo = []
+db = sqlite3.connect('data.db')
 
 p_fname = re.compile('.*?الاسم : (.*)?مكان.*', re.DOTALL)
 p_lname = re.compile('.*?اللقب : (.*)?الاسم.*', re.DOTALL)
@@ -18,20 +19,13 @@ p_birth_place = re.compile('.*?مكان الميلاد : (.*)?تاريخ.*', re.
 p_field = re.compile('.*?الشعبة : (.*)?اللقب.*', re.DOTALL)
 p_grade = re.compile('.*?المعدل : (.*)?الملاحظة.*', re.DOTALL)
 
-done = False
-
 
 def run(thread_name):
-    global done
-    global tabo
-    while len(tabo) < 9999999:
+    while True:
         data = {
-            'matriculebac': str(random.randint(30000000, 39999999)),
+            'matriculebac': str(random.randint(31000000, 39999999)),
             'dobac' : "استظهار+النتيجة"
         }
-        if data['matriculebac'] in tabo:
-            continue
-        tabo.append(data['matriculebac'])
         try:
             r = requests.post(url, data=data).text
         except Exception:
@@ -44,13 +38,25 @@ def run(thread_name):
         m_field = p_field.match(r)
         m_grade = p_grade.match(r)
         if m_fname:
-            print('{}, {},{},{},{}, {}, {}'.format(data['matriculebac'],
-                                                   m_fname.group(1).replace('\\n', ''),
-                                                   m_lname.group(1).replace('\\n', ''),
-                                                   m_birth_date.group(1).replace('\\n', ''),
-                                                   m_birth_place.group(1).replace('\\n', ''),
-                                                   m_field.group(1).replace('\\n', ''),
-                                                   m_grade.group(1).replace('\\n', '')))
+            try:
+                db.execute('''INSERT INTO grades VALUES(?,?,?,?,?,?,?)''', 
+                           (data['matriculebac'],
+                            m_fname.group(1).replace('\\n', ''),
+                            m_lname.group(1).replace('\\n', ''),
+                            m_birth_date.group(1).replace('\\n', ''),
+                            m_birth_place.group(1).replace('\\n', ''),
+                            m_field.group(1).replace('\\n', ''),
+                            m_grade.group(1).replace('\\n', '')))
+                db.commit()
+                print('{}, {},{},{},{}, {}, {}'.format(data['matriculebac'],
+                                                       m_fname.group(1).replace('\\n', ''),
+                                                       m_lname.group(1).replace('\\n', ''),
+                                                       m_birth_date.group(1).replace('\\n', ''),
+                                                       m_birth_place.group(1).replace('\\n', ''),
+                                                       m_field.group(1).replace('\\n', ''),
+                                                       m_grade.group(1).replace('\\n', '')))
+            except Exception:
+                pass
 
 
 if __name__ == '__main__':
