2dc18d170fdbeda44662c6d04cc09e3285f577ca
IOAyman <theycallmethedr@gmail.com>
2016-07-15 12:50:16 +0100

    merged fork into main

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
-#!/bin/python
+#!/bin/python3
 # -*- coding: utf-8 -*-
 
 import re
 import sqlite3
 import threading
 import time
+from math import ceil, sqrt
 
 import requests
 
 
-url = 'http://bac.onec.dz/index.php'
-# To start from this id in order
-mat = 31116933
+URL = 'http://bac.onec.dz/index.php'
+id_go = 39064553  # start from this id in order
+# id_end = 39069999  # last id to fetch in order
+id_count = 100  # how many id to get
+lock = threading.RLock()  # mutex
 
 p_fname = re.compile('.*?الاسم : (.*)?مكان.*', re.DOTALL)
 p_lname = re.compile('.*?اللقب : (.*)?الاسم.*', re.DOTALL)
@@ -22,62 +25,89 @@ p_field = re.compile('.*?الشعبة : (.*)?اللقب.*', re.DOTALL)
 p_grade = re.compile('.*?المعدل : (.*)?الملاحظة.*', re.DOTALL)
 
 
-def run():
-  global mat
+def run(n):
+  global id_go
+
   db = sqlite3.connect('data.db')
-  db.execute("PRAGMA busy_timeout = 300000")  # To avoid db locked exception
-  while mat <= 39050000:
-    m = mat
-    mat += 1
-    data = {
-      'matriculebac': str(m),
-      'dobac': "استظهار+النتيجة"
-    }
+  db.execute("PRAGMA busy_timeout = 300000")  # avoid db locked exception
+
+  i = 0
+  while i < n:
+    i += 1
+
+    # locking out id_go for concurrent access
+    lock.acquire()
+    id_current = id_go
+    id_go += 1
+    lock.release()
+
+    # post data model
+    data = {'matriculebac': str(id_current), 'dobac': "استظهار+النتيجة"}
+
     try:
-      r = requests.post(url, data=data).text
+      r = requests.post(URL, data=data).text
     except Exception:
       time.sleep(2)
+      print('Couldnt fetch {}'.format(id_current))
       continue
-    m_fname = p_fname.match(r)
-    m_lname = p_lname.match(r)
-    m_birth_date = p_birth_date.match(r)
-    m_birth_place = p_birth_place.match(r)
-    m_field = p_field.match(r)
-    m_grade = p_grade.match(r)
-    if m_fname:
+
+    # extract data
+    fname = p_fname.match(r)
+    lname = p_lname.match(r)
+    birth_date = p_birth_date.match(r)
+    birth_place = p_birth_place.match(r)
+    field = p_field.match(r)
+    grade = p_grade.match(r)
+    if fname:
       try:
         db.execute('''INSERT INTO grades VALUES(?,?,?,?,?,?,?)''',
                    (data['matriculebac'],
-                    m_fname.group(1).replace('\\n', ''),
-                    m_lname.group(1).replace('\\n', ''),
-                    m_birth_date.group(1).replace('\\n', ''),
-                    m_birth_place.group(1).replace('\\n', ''),
-                    m_field.group(1).replace('\\n', ''),
-                    m_grade.group(1).replace('\\n', '')
+                    fname.group(1).replace('\\n', ''),
+                    lname.group(1).replace('\\n', ''),
+                    birth_date.group(1).replace('\\n', ''),
+                    birth_place.group(1).replace('\\n', ''),
+                    field.group(1).replace('\\n', ''),
+                    grade.group(1).replace('\\n', '')
                     ))
         db.commit()
         print('{}, {},{},{},{}, {}, {}'
               .format(data['matriculebac'],
-                      m_fname.group(1).replace('\\n', ''),
-                      m_lname.group(1).replace('\\n', ''),
-                      m_birth_date.group(1).replace('\\n', ''),
-                      m_birth_place.group(1).replace('\\n', ''),
-                      m_field.group(1).replace('\\n', ''),
-                      m_grade.group(1).replace('\\n', '')))
+                      fname.group(1).replace('\\n', ''),
+                      lname.group(1).replace('\\n', ''),
+                      birth_date.group(1).replace('\\n', ''),
+                      birth_place.group(1).replace('\\n', ''),
+                      field.group(1).replace('\\n', ''),
+                      grade.group(1).replace('\\n', '')))
+
       except sqlite3.OperationalError:
         print('db locked waiting 5s')
         time.sleep(5)
+
       except Exception as e:
-        pass
-        # print(str(e))
-        print('{} exists'.format(data['matriculebac']))
+        # pass
+        print(str(e))
+        # print('{} exists'.format(data['matriculebac']))
+
+    else:
+      print('not found {}'.format(data['matriculebac']))
 
 
 if __name__ == '__main__':
-  threads = [threading.Thread(target=run) for i in range(5)]
+  # number of total IDs to fetch
+  id_total = id_end - id_go \
+    if (('id_end' in globals()) and (id_end > id_go)) \
+    else id_count
 
-  for thread in threads:
+  # scale number of threads according to requests number
+  threads_count = ceil(sqrt(id_total))
+
+  print('Total IDs to fetch: {}, using {} threads'
+        .format(id_total, threads_count))
+
+  thrds = [threading.Thread(
+    target=run,
+    # all threads get the same amount of requests to make
+    args=(int(id_total / threads_count),)) for i in range(threads_count)]
+
+  for thread in thrds:
     thread.start()
-
-  for thread in threads:
-    thread.join()
